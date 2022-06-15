53923a984ace598345307137407b3e332da3bca0
gaiahinds <102543051+WAaronD@users.noreply.github.com>
2022-06-15 08:41:41 -0700

    Add code to map sound speed
    Still based on potential temperature rather than in situ temp -- might need to fix that

--- plotSSP.m
+++ plotSSP.m
@@ -2,6 +2,7 @@
 % Plot Sound Speed Profile for region of choice.
 
 % AD - in development
+% Currently configured for use with HYCOM's files
 
 clearvars
 close all
@@ -32,15 +33,46 @@ longtwelfths = 12*longlims;
 
 layer1 = saldat(longtwelfths(1):longtwelfths(2),lattwelfths(1):lattwelfths(2),1);
 
+%% Map salinity and temperature at surface
 figure(1) % Map salinity @ 1m
-layer1 = saldat(1:400,2550:3000,1);
-heatmap(layer1, 'Colormap', turbo)
+layersal = saldat(1:400,2550:3000,1);
+heatmap(layersal, 'Colormap', turbo)
 grid off
 
+%NOTE: temperature data provides potential temperature which may not be
+%what we want
 figure(2) % Map potential temperature @ 1m
-layer1 = tempdat(1:400,2550:3000,1);
-heatmap(layer1, 'Colormap', turbo)
+layertemp = tempdat(1:400,2550:3000,1);
+heatmap(layertemp, 'Colormap', turbo)
 grid off
 
+%% Merge data to sound speed
+% Development goal: Get 3D grid of sound speeds
 
-ncdisp([NCfilePath,'\','rarchv.2018_331_00_2d.nc']);
+maxdepth = 11;
+dataset_size = (lattwelfths(2) - lattwelfths(1))*(longtwelfths(2) - longtwelfths(1))*maxdepth;
+
+%format of saldat is lat, long, depth
+%format of tempdat is lat, long, depth
+
+%generate salinity and temperature matrices for region
+saldat_reg = saldat(1:400,2550:3000,1:11);
+tempdat_reg = tempdat(1:400,2550:3000,1:11);
+% create analogous "depthdat"
+depthdat_reg = zeros(400, 451, 11);
+for i=1:maxdepth
+    depthdat_reg(1:400,1:451,i) = i;
+end
+
+cdat = salt_water_c(tempdat_reg, depthdat_reg, saldat_reg);
+
+%% Map sound speed
+figure(3) % Map sound speed @ 1m
+layerc = cdat(1:400,1:451,1);
+heatmap(layerc, 'Colormap', turbo)
+grid off
+
+figure(4) % Map sound speed @ 10m
+layerc = cdat(1:400,1:451,10);
+heatmap(layerc, 'Colormap', turbo)
+grid off
