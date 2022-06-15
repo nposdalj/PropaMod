e3208ff8ed01e0ece1d4139448c2f65854a83af1
gaiahinds <102543051+WAaronD@users.noreply.github.com>
2022-06-15 10:38:18 -0700

    Add new code to incorporate deeper depths

--- plotSSP.m
+++ plotSSP.m
@@ -4,13 +4,18 @@
 % AD - in development
 % Currently configured for use with HYCOM's files
 
+% NOTE: HYCOM data provides data points for every 1/12 degree.I believe 
+% that is at most every ~9.25 km. This MIGHT allow us to see significant
+% differences in sound speed across a 20-km range, but given such a low
+% resolution, I think those differences will be somewhat imprecise.
+
 clearvars
 close all
 
 %% Parameters defined by user
 filePrefix = 'OS_CCE1'; % File name to match.
 siteabrev = 'Global';
-FilePath = 'C:\Users\HARP\Documents\MATLAB\Propagation Modelling';
+FilePath = 'C:\Users\HARP\Documents\MATLAB\Propagation_Modelling';
 NCfilePath = [FilePath,'\',siteabrev]; %directory of TPWS files
 salfile   = 'rarchv.2018_331_00_3zs.nc'; % Enter salinity file here
 tempfile  = 'rarchv.2018_331_00_3zt.nc'; % Enter temperature file here
@@ -76,3 +81,48 @@ figure(4) % Map sound speed @ 10m
 layerc = cdat(1:400,1:451,10);
 heatmap(layerc, 'Colormap', turbo)
 grid off
+
+load('C:\Users\HARP\Documents\MATLAB\Propagation_Modelling\0001_20160201T120000');
+temp_structure = D.temperature;
+sal_structure = D.salinity;
+temp_structure = flip(permute(temp_structure, [2 1 3]),1); % To make maps work, swaps lat/long and flips lat
+sal_structure = flip(permute(sal_structure, [2 1 3]),1);
+depth_structure = zeros(301, 238, 40);
+for i=1:301
+    for j=1:238
+        depth_structure(i,j,1:40) = D.Depth;
+    end
+end
+
+cdat = salt_water_c(temp_structure,depth_structure,sal_structure);
+
+figure(5) % Sound Speed at various depths
+subplot(1,4,1) % 0 m (surface)
+heatmap(cdat(1:301,1:238,1), 'Colormap', turbo)
+grid off
+xlabel("0 m")
+subplot(1,4,2) % -300 m (surface)
+heatmap(cdat(1:301,1:238,25), 'Colormap', turbo)
+grid off
+xlabel("300 m")
+subplot(1,4,3) % -600 m (surface)
+heatmap(cdat(1:301,1:238,29), 'Colormap', turbo)
+grid off
+xlabel("600 m")
+subplot(1,4,4) % -1000 m (surface)
+heatmap(cdat(1:301,1:238,33), 'Colormap', turbo)
+grid off
+xlabel("1000 m")
+
+Xtix = 1:238;
+reducedXtix = string(Xtix);
+reducedXtix(mod(Xtix,30) ~= 0) = "";
+Ytix = 1:301;
+reducedYtix = string(Ytix);
+reducedYtix(mod(Ytix,30) ~= 0) = "";
+
+depth0 = heatmap(cdat(1:301,1:238,1), 'Colormap', turbo);
+grid off
+xlabel("0 m")
+depth0.XDisplayLabels = reducedXtix;
+depth0.YDisplayLabels = reducedYtix;
