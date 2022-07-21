3cb46e179e96c9c6bc80605420d8e46febdbd405
gaiahinds <102543051+WAaronD@users.noreply.github.com>
2022-07-21 09:12:45 -0700

    Calculate extreme months

--- SSP_Seasonality.R
+++ SSP_Seasonality.R
@@ -2,7 +2,7 @@
 # Based on data from HYCOM
 
 #### Parameters defined by user ####
-siteabrev <- "NC"
+siteabrev <- "GS"
 region <- "WAT"
 depthlist_range = 1:33 # Depth levels you would like to analyze (NOT the same as the actual depths!!)
 
@@ -30,12 +30,15 @@ SSP_M09 <- rowMeans(SSP_All[12*(0:3)+4], na.rm=TRUE)  # Sep
 SSP_M10 <- rowMeans(SSP_All[12*(0:3)+5], na.rm=TRUE)  # Oct
 SSP_M11 <- rowMeans(SSP_All[12*(0:3)+6], na.rm=TRUE)  # Nov
 SSP_M12 <- rowMeans(SSP_All[12*(0:3)+7], na.rm=TRUE)  # Dec
+SSP_MoAvgs <- cbind(SSP_M01,SSP_M02,SSP_M03,SSP_M04,SSP_M05,SSP_M06,
+                    SSP_M07,SSP_M08,SSP_M09,SSP_M10,SSP_M11,SSP_M12)
 
 #### Graphical Analysis ####
 
 # Plot mean SSP of the first day of each month
 x_min <- min(SSP_All[depthlist_range,2:49], na.rm=TRUE)-5 # Graph x-limits
 x_max <- max(SSP_All[depthlist_range,2:49], na.rm=TRUE)+5
+
 plot(SSP_M01[depthlist_range],-depthlist[depthlist_range], 'l', col="#0080FF",   # Jan - blue
      xlim=c(x_min,x_max),
      ylim=c(-depthlist[max(depthlist_range)],-depthlist[min(depthlist_range)]), 
@@ -45,12 +48,12 @@ points(SSP_M03[depthlist_range],-depthlist[depthlist_range], 'l', col="#8000FF")
 points(SSP_M04[depthlist_range],-depthlist[depthlist_range], 'l', col="#FF00FF") # April - Pink
 points(SSP_M05[depthlist_range],-depthlist[depthlist_range], 'l', col="#FF0080")
 points(SSP_M06[depthlist_range],-depthlist[depthlist_range], 'l', col="#FF0000")
-points(SSP_M07[depthlist_range],-depthlist[depthlist_range], 'l', col="#FF8000")
-points(SSP_M08[depthlist_range],-depthlist[depthlist_range], 'l', col="#FFFF00") # July - Orange
+points(SSP_M07[depthlist_range],-depthlist[depthlist_range], 'l', col="#FF8000") # July - Orange
+points(SSP_M08[depthlist_range],-depthlist[depthlist_range], 'l', col="#FFFF00")
 points(SSP_M09[depthlist_range],-depthlist[depthlist_range], 'l', col="#80FF00")
-points(SSP_M10[depthlist_range],-depthlist[depthlist_range], 'l', col="#00FF00")
+points(SSP_M10[depthlist_range],-depthlist[depthlist_range], 'l', col="#00FF00") # October - Green
 points(SSP_M11[depthlist_range],-depthlist[depthlist_range], 'l', col="#00FF80")
-points(SSP_M12[depthlist_range],-depthlist[depthlist_range], 'l', col="#00FFFF") # October - Green
+points(SSP_M12[depthlist_range],-depthlist[depthlist_range], 'l', col="#00FFFF")
 
 # Plot mean SSP across all months with standard deviation
 SSP_MAnnual <- rowMeans(SSP_All[2:49], na.rm=TRUE)
@@ -69,25 +72,34 @@ arrows(SSP_MAnnual-SSP_stdev,-depthlist,SSP_MAnnual+SSP_stdev,-depthlist,
 sumtab <- cbind(depthlist,SSP_MAnnual,SSP_stdev)
 colnames(sumtab) <- c("Depth","c_mean","c_stdev")
 
+
 #Calculate params at 800m
 M800 <- c(SSP_M01[31],SSP_M02[31],SSP_M03[31],SSP_M04[31],SSP_M05[31],SSP_M06[31],
           SSP_M07[31],SSP_M08[31],SSP_M09[31],SSP_M10[31],SSP_M11[31],SSP_M12[31])
 range800 <- range(M800)
-min_month <- which(M800 == range800[1])
-max_month <- which(M800 == range800[2])
+min_mo800 <- which(M800 == range800[1])
+max_mo800 <- which(M800 == range800[2])
 
-sumtab800 <- cbind(depthlist[31],SSP_MAnnual[31],SSP_stdev[31],range800[1],min_month,range800[2],max_month)
-colnames(sumtab800) <- c("Depth","c_mean","c_stdev","c_min","min_mo","c_max","max_mo")
+sumtab800 <- cbind(depthlist[31],SSP_MAnnual[31],SSP_stdev[31],min_mo800,range800[1],max_mo800,range800[2])
+colnames(sumtab800) <- c("Depth","c_mean","c_stdev","min_mo800","c_min","max_mo800","c_max")
 
-#Calculate month with min and month with max
+#Calculate months where average sound speeds across depths 200m+ are max and min
+MoAvgs_200plus <- colMeans(SSP_MoAvgs[23:40,], na.rm=TRUE)
+range_MoAvg <- range(MoAvgs_200plus)
+min_mo = which(MoAvgs_200plus == range_MoAvg[1])
+max_mo = which(MoAvgs_200plus == range_MoAvg[2])
+sumtab_extremes <- cbind(min_mo,max_mo)
+rownames(sumtab_extremes) <- "Month"
 
 # Save text file with output
 filename = paste(saveDir,'/',siteabrev,'_SSP_SeasonalStats.txt',sep="")
 sink(filename)
 sumtab
+sink(file = NULL)
+
 summary(sumtab)
 sumtab800
-sink(file = NULL)
+sumtab_extremes
 
 # At 800m, save the mean sound speed, the range (min/max), and standard deviation
 # What two months were the extremes (we might use this for propagation modeling sensitivity test)
