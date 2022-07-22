8fb90c052d141d4c3a759f480024ddd800df1d35
Natalie Posdaljian <nposdalj@ucsd.edu>
2022-07-22 13:57:59 -0700

    Added line for me

--- pDetSim_constructWS.m
+++ pDetSim_constructWS.m
@@ -17,6 +17,7 @@ sp = 'Pm';
 GDrive = 'I';
 inputDir = [GDrive,':\My Drive\PropagationModeling\Radials\',Site]; % Where your data is coming from
 exportDir = [GDrive,':\My Drive\PropagationModeling\DetSim_Workspace\',Site]; % Where the assembled workspace will be saved
+inputDir = 'C:\Users\nposd\Desktop\PropagationModelingIntermediate\220721151649';
 %% Load workspace from bellhopDetRange to extract nrr and rr
 load(fullfile(exportDir, [Site,'_bellhopDetRange.mat']))
 %% Loop through .shd files and extract depth and transmission loss
