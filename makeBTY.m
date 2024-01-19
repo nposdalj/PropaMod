bc62a68edcdf8b62876e0167431054e0d82d66cc
Aaron Deans <102543051+WAaronD@users.noreply.github.com>
2024-01-18 19:35:15 -0800

    synced JAH's changes to our makeBTY; removed JAH's version

--- makeBTY.m
+++ makeBTY.m
@@ -6,9 +6,14 @@ global loni
 global rad
 
 % Find which cell contains lat, lon, and elevation/depth
-lonLoc = (find(strcmp(AllVariables,'lon'))+1)/2; % Find which cell contains lon
-latLoc = (find(strcmp(AllVariables,'lat'))+1)/2; % Find which cell contains lat
-elevationLoc = (find(strcmp(AllVariables,'elevation'))+1)/2; % Find which cell contains elevation
+% The following works for JAH's files
+lonLoc = (find(strcmp(AllVariables,'x'))+1)/2; % Find which cell contains lon
+latLoc = (find(strcmp(AllVariables,'y'))+1)/2; % Find which cell contains lat
+elevationLoc = (find(strcmp(AllVariables,'z'))+1)/2; % Find which cell contains elevation
+% And the following for WASD/NP's files
+% lonLoc = (find(strcmp(AllVariables,'lon'))+1)/2; % Find which cell contains lon
+% latLoc = (find(strcmp(AllVariables,'lat'))+1)/2; % Find which cell contains lat
+% elevationLoc = (find(strcmp(AllVariables,'elevation'))+1)/2; % Find which cell contains elevation
 
 % Extract lat, lon, and elevation for radial
 latRange = [hlat+1 hlat-1]; %+/- 1 degree for lat
@@ -20,28 +25,30 @@ lonVals = AllVariables{2,lonLoc}(lonValsIDX);
 
 depthVals = AllVariables{2,elevationLoc}(lonValsIDX,latValsIDX); %index depth based on +/- lat and lon away from reciever
 
-%create two columns for lat and lon
-[X,Y] = meshgrid(latVals,lonVals);
-latlon = [X(:),Y(:),];
-
-%Find the first and last sequence for every nth value for blocking of depth
-nth = height(latVals);
-sequFIRST = 1:nth:height(latlon);
-sequLAST = nth:nth:height(latlon);
-
-%Take each row of depth and transpose it as a column to match the lat/lon matrix
-for ii = 1:height(depthVals)
-    Row = -1*depthVals(:,ii);
-    latlon(sequFIRST(ii):sequLAST(ii),3) = Row;
-end
+% %create two columns for lat and lon
+% [X,Y] = meshgrid(latVals,lonVals);
+% latlon = [X(:),Y(:),];
+% 
+% %Find the first and last sequence for every nth value for blocking of depth
+% nth = height(latVals);
+% sequFIRST = 1:nth:height(latlon);
+% sequLAST = nth:nth:height(latlon);
+% 
+% %Take each row of depth and transpose it as a column to match the lat/lon matrix
+% for ii = 1:height(depthVals)
+%     Row = -1*depthVals(:,ii);
+%     latlon(sequFIRST(ii):sequLAST(ii),3) = Row;
+% end
 %% Interpolate w/ interp2 to extract a line of bathymetry?
 fpn = fullfile(fpath, [fname, '.bty']);
 [xi, yi] = latlon2xy(lati(rad, :), loni(rad, :), hlat, hlon);
 ri = sqrt(xi.^2 + yi.^2)./1000;
-bathi = griddata(latlon(:,1), latlon(:,2), latlon(:,3), lati(rad, :), loni(rad, :), 'linear');
+% bathi = griddata(latlon(:,1), latlon(:,2), latlon(:,3), lati(rad, :), loni(rad, :), 'linear');
+% JAH
+bathi = interp2(latVals, lonVals, double(depthVals), lati(rad,:), loni(rad,:));
 
-Range = ri.';
-bath = bathi.'; % bath = abs(bathi).'; % Old ver
+Range = ri';
+bath = bathi'; % bath = abs(bathi).'; % Old ver
 
 [Rsort, I] = sort(Range);
 bathSort = bath(I);
